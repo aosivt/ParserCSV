cb91d5cb286110e5ab91c5df87d95b5012bd18db
Alex <aosivt@gmail.com>
2019-01-17 17:40:22 +0700

    fix builderProducts

--- src/main/java/aos/covergame/builder/BuilderProducts.java
+++ src/main/java/aos/covergame/builder/BuilderProducts.java
@@ -1,22 +1,17 @@
 package aos.covergame.builder;
 
 import aos.covergame.model.Product;
-import org.apache.commons.io.FilenameUtils;
-
 import java.io.*;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+
 import java.util.*;
 import java.util.function.Function;
-import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
 public class BuilderProducts {
 
-    private static String TYPE_FILE = "csv";
-
-//    final PriorityQueue<Product> products = new PriorityQueue<>();
     private volatile ProductCollection products = new ProductCollection(100);
 
     private BuilderProducts(){}
@@ -31,7 +26,6 @@ public class BuilderProducts {
                     .parallel()
                                 .map(Path::toFile)
                                 .filter(File::isFile)
-                                .filter(builderProducts::isCsv)
                                 .map(builderProducts.bufferedReader)
                                 .forEach((bReader) -> {
                                     try {
@@ -68,31 +62,20 @@ public class BuilderProducts {
         products.add(product);
     }
 
-    private boolean isCsv(File file) {
-        boolean isCsv = false;
-        try {
-            isCsv = FilenameUtils.getExtension(file.getName()).equals(TYPE_FILE);
-        } catch (NullPointerException e) {
-            e.printStackTrace();
-        }
-        return isCsv;
-    }
-    private void closeBufferedReader (BufferedReader br){
-        try {
-            br.close();
-        }catch (IOException e){
-            e.printStackTrace();
-        }
-    }
     private Function<String, Product> mapToItem = (line) -> {
-        String[] p = line.split(";");
-        Product item = new Product();
-        item.setProductID(Integer.parseInt(p[0]));
-        item.setName(p[1]);
-        item.setCondition(p[2]);
-        item.setState(p[3]);
-        item.setPrice(Float.parseFloat(p[4]));
-        return item;
+        try{
+            String[] p = line.split(";");
+            Product item = new Product();
+            item.setProductID(Integer.parseInt(p[0]));
+            item.setName(p[1]);
+            item.setCondition(p[2]);
+            item.setState(p[3]);
+            item.setPrice(Float.parseFloat(p[4]));
+            return item;
+        }catch (NumberFormatException e){
+            e.printStackTrace();
+        }
+        return null;
     };
 
     public ProductCollection getProducts() {
